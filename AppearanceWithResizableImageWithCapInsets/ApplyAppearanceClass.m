701614d05672abbdcd939698277b4a1ac313cd75
 azu <azuciao@gmail.com>
2013-01-30 11:19:45 +0900

    correct UIAppearance

--- AppearanceWithResizableImageWithCapInsets/ApplyAppearanceClass.m
+++ AppearanceWithResizableImageWithCapInsets/ApplyAppearanceClass.m
@@ -12,13 +12,17 @@
 
 }
 + (void)applyDefaultAppearance {
-    UIImage *navigationBackgroundImage = [UIImage imageNamed:@"navigationbar.png"];
+    // ナビゲーションバーの背景画像
+    UIImage *navigationBackgroundImage = [[UIImage imageNamed:@"navigationbar.png"]
+            resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 5, 0)];
     [[UINavigationBar appearance] setBackgroundImage:navigationBackgroundImage forBarMetrics:UIBarMetricsDefault];
     // ナビゲーションバーアイテムのカスタマイズ
-    UIImage *rightButtonImage = [[UIImage imageNamed:@"button_navigation_right.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 5, 0, 5)];
+    UIImage *rightButtonImage = [[UIImage imageNamed:@"button_navigation_right.png"]
+            resizableImageWithCapInsets:UIEdgeInsetsMake(5, 5, 5, 5)];
     [[UIBarButtonItem appearanceWhenContainedIn:[UINavigationBar class], nil]
             setBackgroundImage:rightButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
-    UIImage *leftButtonImage = [[UIImage imageNamed:@"button_navigation_left.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 5, 0, 5)];
+    UIImage *leftButtonImage = [[UIImage imageNamed:@"button_navigation_left.png"]
+            resizableImageWithCapInsets:UIEdgeInsetsMake(5, 15, 5, 5)];
     [[UIBarButtonItem appearanceWhenContainedIn:[UINavigationBar class], nil]
             setBackButtonBackgroundImage:leftButtonImage forState:UIControlStateNormal barMetrics:UIBarMetricsDefault];
 
